c8cec93ed53a08eb3e1209563676988c244553a9
BONNIES-PC\mattpigram <mattpigram90@hotmail.com>
2023-02-25 21:45:34 +1100

    Removed some redundant import code, added canvas element for the three renderer to attach to

--- src/ts/World.ts
+++ src/ts/World.ts
@@ -1,5 +1,4 @@
-import * as THREE from 'three';
-import { Camera, Scene, WebGLRenderer } from 'three';
+import { PerspectiveCamera, Camera, Scene, WebGLRenderer } from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 
@@ -18,15 +17,15 @@ export class World {
             throw "Error, no target DOM element supplied for canvas to be created";
         }
 
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.scene = new Scene();
+        this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.camera.position.z = 5;
-        this.renderer = new THREE.WebGLRenderer();
+        this.renderer = new WebGLRenderer({
+            canvas: canvasTarget
+        });
         this.composer = new EffectComposer(this.renderer);
 
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        
-        canvasTarget.appendChild(this.renderer.domElement);
+        this.renderer.setSize(window.innerWidth * .90, window.innerHeight);
 
         const renderPass = new RenderPass(this.scene, this.camera);
         this.composer.addPass(renderPass);
